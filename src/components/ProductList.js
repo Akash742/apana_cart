7b81ff446398dba435ae6adb493106bf8aefdadd
Akash742 <akashdey742@gmail.com>
2022-04-27 13:43:04 +0530

    deploy apna_cart to github

---
+++ src/components/ProductList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Product from "./Product.js";
+
+export default function ProductList(props) {
+  return props.productList.length > 0
+    ? props.productList.map((product, i) => {
+        return (
+          <Product
+            product={product}
+            key={i}
+            incrementQuantity={props.incrementQuantity}
+            decrementQuantity={props.decrementQuantity}
+            index={i}
+            removeItem={props.removeItem}
+          />
+        );
+      })
+    : <h1>No Product is present in the Cart.</h1>;
+}
